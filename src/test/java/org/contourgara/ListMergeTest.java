1f446e9548c34abea2dcbf714142d60f37352a01
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-11-07 12:56:28 +0900

    検証完了

---
+++ src/test/java/org/contourgara/ListMergeTest.java
@@ -0,0 +1,92 @@
+package org.contourgara;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+import org.apache.commons.collections4.ListUtils;
+import org.apache.commons.collections4.SetUtils;
+import org.junit.jupiter.api.Test;
+
+class ListMergeTest {
+    @Test
+    void addAllはインスタンスが変わらない() {
+        // setup
+        Map<String, List<Integer>> map = new HashMap<>();
+        map.put("a", new ArrayList<>(List.of(1, 2, 3)));
+        map.put("b", new ArrayList<>(List.of(4, 5, 6)));
+
+        Map<String, List<Integer>> updatedMap = new HashMap<>();
+        updatedMap.put("a", new ArrayList<>(List.of(1, 2, 3)));
+        updatedMap.put("b", new ArrayList<>(List.of(4, 5, 6)));
+
+        List<Integer> list1 = updatedMap.get("a");
+        List<Integer> list2 = List.of(4, 5, 6);
+
+        // execute
+        list1.addAll(list2);
+
+        // assert
+        assertThat(list1).isEqualTo(updatedMap.get("a"));
+        assertThat(updatedMap).isNotEqualTo(map);
+    }
+
+    @Test
+    void streamで連結すれば新規リストが作成される() {
+        // setup
+        Map<String, List<Integer>> map = new HashMap<>();
+        map.put("a", new ArrayList<>(List.of(1, 2, 3)));
+        map.put("b", new ArrayList<>(List.of(4, 5, 6)));
+
+        List<Integer> list1 = map.get("a");
+        List<Integer> list2 = List.of(4, 5, 6);
+
+        // execute
+        List<Integer> list3 = Stream.concat(list1.stream(), list2.stream()).toList();
+
+        // assert
+        assertThat(list3).isNotEqualTo(map.get("a"));
+        System.out.println(map);
+    }
+
+    @Test
+    void ListUtilsを使用すれば新規リストが作成される() {
+        // setup
+        Map<String, List<Integer>> map = new HashMap<>();
+        map.put("a", new ArrayList<>(List.of(1, 2, 3)));
+        map.put("b", new ArrayList<>(List.of(4, 5, 6)));
+
+        List<Integer> list1 = map.get("a");
+        List<Integer> list2 = List.of(4, 5, 6);
+
+        // execute
+        List<Integer> list3 = ListUtils.union(list1, list2);
+
+        // assert
+        assertThat(list3).isNotEqualTo(map.get("a"));
+        System.out.println(map);
+    }
+
+    @Test
+    void SetUtilsを使用すれば新規セットが作成される() {
+        // setup
+        Map<String, Set<Integer>> map = new HashMap<>();
+        map.put("a", new HashSet<>(List.of(1, 2, 3)));
+        map.put("b", new HashSet<>(List.of(4, 5, 6)));
+
+        Set<Integer> set1 = map.get("a");
+        Set<Integer> set2 = Set.of(4, 5, 6);
+
+        // execute
+        Set<Integer> set3 = SetUtils.union(set1, set2);
+
+        // assert
+        assertThat(set3).isNotEqualTo(map.get("a"));
+        System.out.println(map);
+    }
+}
